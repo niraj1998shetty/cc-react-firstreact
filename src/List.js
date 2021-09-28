6dbb617f28fd0f623c699326b55fd5b59cacb9bc
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2021-09-28 18:26:48 +0530

    Add files via upload

---
+++ src/List.js
@@ -0,0 +1,15 @@
+
+
+const Lists= ()=>{
+return (<>
+<ul>
+    <li>onlne delivery food</li>
+    <li>delicious</li>
+    <li>italian</li>
+    <li>north</li>
+    <li>south food</li>
+</ul>
+</>)
+}
+
+export default Lists;
